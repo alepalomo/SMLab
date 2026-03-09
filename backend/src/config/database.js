ea15cdfe7f045e2a692c11e33cb91f5c0a6fc9a5
Alejandro jose Palomo García <alejandrojosepalomogarcia@MacBook-Pro-de-Alejandro.local>
2026-03-09 11:35:25 -0600

    fix db internal connection

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,12 +4,9 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+  dialectOptions: process.env.DATABASE_URL?.includes('railway.internal') 
+    ? {} 
+    : { ssl: { require: true, rejectUnauthorized: false } }
 });
 
 module.exports = sequelize;
